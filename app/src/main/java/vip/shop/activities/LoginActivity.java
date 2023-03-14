c4ada1858154eab1d8e11b0f352aa0d2912fcfe5
nhuocvu <toanlxag5572001123@gmail.com>
2023-03-15 01:20:00 +0700

    update

--- app/src/main/java/vip/shop/activities/LoginActivity.java
+++ app/src/main/java/vip/shop/activities/LoginActivity.java
@@ -8,6 +8,8 @@ import vip.shop.R;
 import vip.shop.databinding.ActivityLoginBinding;
 import vip.shop.databinding.ActivityMainBinding;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
 ActivityLoginBinding binding;
@@ -15,5 +17,16 @@ ActivityLoginBinding binding;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        //nút hiện ẩn pass. chưa có phương thức get pass nên chưa hoàn thiện phần này hoàn thiện khi làm xong full login
+//        ImageView imageViewShowHidePass = findViewById(R.id.show_hidepass);
+//        imageViewShowHidePass.setImageResource(R.drawable.ic_hide_pwd);
+//        imageViewShowHidePass.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
     }
+
+
 }
--- app/src/main/java/vip/shop/activities/MainActivity.java
+++ app/src/main/java/vip/shop/activities/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity  {
                 binding.rcCategory.setAdapter(adapter);
             }
         });
+
     }
 
 }
